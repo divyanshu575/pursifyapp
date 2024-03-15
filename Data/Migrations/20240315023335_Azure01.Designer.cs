751b4acf5f27139c07ec59e80c579084b87cc2ba
Divyanshu Sharma <W0850261@myscc.ca>
2024-03-14 22:36:00 -0400

    Azure

--- Data/Migrations/20240315023335_Azure01.Designer.cs
+++ Data/Migrations/20240315023335_Azure01.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using pursifyapp.Data;
 
@@ -11,9 +12,11 @@ using pursifyapp.Data;
 namespace pursifyapp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240315023335_Azure01")]
+    partial class Azure01
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
